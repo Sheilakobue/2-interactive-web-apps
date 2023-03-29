10fc4a209e2ed562f9a308cfc5e901f3199ce931
Sheila Kobue <maserole.mojela@gmail.com>
2023-03-29 15:42:24 +0200

    added comment

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA3/IWA_3.4 Challenge 2/src/alex.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA3/IWA_3.4 Challenge 2/src/alex.js
@@ -1,6 +1,8 @@
 // alex.js
 //exported the variables
 //added const
+//removed private from the variables
+
 export const firstName = "Alex"
 export const surname = "Naidoo"
 export const role = "Head of Marketing"
