e49bdfd03acc03a1a740c8cf7acc2d23726c8626
Sheila Kobue <maserole.mojela@gmail.com>
2023-03-29 10:00:09 +0200

    changed my comment

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA2/IWA_2.6 Challenge 3/src/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA2/IWA_2.6 Challenge 3/src/script.js
@@ -7,6 +7,6 @@
 
 console.warn("Security scan starting")
 
-// It is important to let user know when they can close the page  /*
+// It is important to let user know when they can close the page  
 
 console.info("Please wait for scan to complete before closing the browser.")
