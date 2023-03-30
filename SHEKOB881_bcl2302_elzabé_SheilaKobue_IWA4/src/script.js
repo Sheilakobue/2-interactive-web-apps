c36c2b566161460ad6cdbef5532037bced2bb6d8
Sheila Kobue <maserole.mojela@gmail.com>
2023-03-30 14:14:24 +0200

    added the add date

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA4/src/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA4/src/script.js
@@ -1,37 +1,51 @@
-// changed : to = sign
-// changed const to let
-// removed 2050 from the if statement
-
+/*
+ changed cost to let
+ added the ;(semicolons)
+ changed :(punctuation) to =(equal sign)
+ */
 let date = 2050;
 let stat = 'student';
 let count = 0;
+
+/*
+first if statement 
+added =(equal signs)
+added the ;(semicolons)
+removed date='april'
+added "april" in the console.log
+removed let count =, and added =
+and closed ;(semicolon)
+*/
 
 if (date === 2050) 
 {
 	console.log("January:", 'New Year’s Day');
 	console.log("March:", 'Human Rights Day');
-	console.log("April:", 'Family Day');
-	console.log("April:", 'Freedom Day');
+	let date = 'April:'
+    console.log(date, 'Family Day');
+	console.log(date, 'Freedom Day');
 	count +=4;
 }
 if (stat === "student") {
     console.log("June:", 'Youth Day')
     count += 1;
-}
 
-console.log("August:", 'Women’s Day')
-	console.log("September:", 'Heritage Day')
-	console.log('December:', 'Day of Reconciliation')
-	count += 3;
-
-   
-	if (stat === "parent") {
-	console.log("December:", 'Christmas Day')
-	count += 1;
-  }
-
+    console.log("August:", 'Women’s Day')
+    console.log("September:", 'Heritage Day')
+    let date = 'December:';
+    console.log(date, 'Day of Reconciliation')
+    count += 3;
+    }
+    
+       
+if (stat === "parent") {
+    console.log(date, 'Christmas Day')
+    count += 1;
+    }
+    
     console.log("December:", 'Day of Goodwill')
     count += 1;
+    
 
 
 console.log('Your status is:', stat)
