3884916189936625caf1b0dfac0e3ab3cabec637
Sheila Kobue <maserole.mojela@gmail.com>
2023-03-30 14:47:19 +0200

    added comment

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA4/src/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA4/src/script.js
@@ -4,47 +4,46 @@
  changed :(punctuation) to =(equal sign)
  */
 let date = 2050;
-let stat = 'student';
+let stat = 'parent';
 let count = 0;
 
 /*
 first if statement 
 added =(equal signs)
 added the ;(semicolons)
-removed date='april'
-added "april" in the console.log
+added let to the date
 removed let count =, and added =
-and closed ;(semicolon)
+and closed with ;(semicolon)
 */
 
-if (date === 2050) 
-{
+if (date===2050) {
 	console.log("January:", 'New Year’s Day');
 	console.log("March:", 'Human Rights Day');
-	let date = 'April:'
+	let date = "April:"
     console.log(date, 'Family Day');
 	console.log(date, 'Freedom Day');
 	count +=4;
-}
-if (stat === "student") {
-    console.log("June:", 'Youth Day')
-    count += 1;
 
-    console.log("August:", 'Women’s Day')
-    console.log("September:", 'Heritage Day')
-    let date = 'December:';
-    console.log(date, 'Day of Reconciliation')
+if (stat === "student") {
+    console.log("June:", 'Youth Day');
+    count += 1;
+}
+
+    console.log("August:", 'Women’s Day');
+    console.log("September:", 'Heritage Day');
+    date = "December:";
+    console.log(date, 'Day of Reconciliation');
     count += 3;
-    }
-    
-       
+ 
+        
 if (stat === "parent") {
+    
     console.log(date, 'Christmas Day')
     count += 1;
-    }
-    
-    console.log("December:", 'Day of Goodwill')
+}   
+    console.log(date, 'Day of Goodwill');
     count += 1;
+}
     
 
 
