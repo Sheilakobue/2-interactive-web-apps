b89d705ce44f34f09cc8da2870f38f68dfaf588e
Sheila Kobue <maserole.mojela@gmail.com>
2023-04-03 09:49:20 +0200

    added another if statement

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA5/IWA_5.2 Challenge 1/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA5/IWA_5.2 Challenge 1/script.js
@@ -2,6 +2,19 @@ const FREE_WARNING = "Free shipping only applies to single customer orders";
 const BANNED_WARNING = "Unfortunately we do not ship to your country of residence";
 const NONE_SELECTED = '0';
 
-if (location === "RSA" && shipping === 400 && currency ==="R"){
+if (FREE-WARNING){
+	location === "RSA" && shipping === 400 && currency ==="R"
       console.log (FREE_WARNING)
 }
+
+if (location === NAM && shipping === 600){
+}else {shipping = 800
+
+shoes = 300 * 1
+toys - 100 * 5
+hirts = 150 * 'NONE_SELECTED'
+batteries 35 * 2
+pens = 5 * 'NONE_SELECTED' 
+
+shipping = null
+currency = $
