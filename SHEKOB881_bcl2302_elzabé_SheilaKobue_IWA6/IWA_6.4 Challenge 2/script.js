d09794b5d3f76242947aa9701ec7b8d8ab975dd4
Sheila Kobue <maserole.mojela@gmail.com>
2023-04-04 20:21:56 +0200

    edited comments

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA6/IWA_6.4 Challenge 2/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA6/IWA_6.4 Challenge 2/script.js
@@ -7,6 +7,14 @@ const hourOfDay = 00;
 const minuteOfDay = 00;
 
 // Only change below this line
+
+// i have used the if statement to state that if the hour of the day and minute =0 then we calculate expense 
+// or else if the the hour of day and minutes are not known(null) then no calculations as the result is undefined.
+
+// i have added ()brackets to the if statement
+// i have changed tax number to number using parseInt
+// multiplied the salary by tax number
+
 if (hourOfDay === 00 && minuteOfDay === 00)
 {
  const taxAsDecimal = parseInt(tax) / 100;
