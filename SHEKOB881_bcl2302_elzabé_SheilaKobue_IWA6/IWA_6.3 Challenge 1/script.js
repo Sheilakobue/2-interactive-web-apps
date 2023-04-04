f1f0efff5cee0abe90dfa22f921056d1cf2af19c
Sheila Kobue <maserole.mojela@gmail.com>
2023-04-04 21:18:20 +0200

    Update script.js

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA6/IWA_6.3 Challenge 1/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA6/IWA_6.3 Challenge 1/script.js
@@ -5,9 +5,8 @@ const secondaryPhone = '0219131568'
 //i've noticed that valuables are not numbers but string
 //i have stated by using !NaN to state that the variables and not a number
 // added && and string to state that primaryPhone is a string.
-
+// constant has been used because the variables are not going be reassigned
 const primaryValid = !isNaN(primaryPhone)&& typeof primaryPhone== 'string';
-
 const secondaryValid = !isNaN(secondaryPhone) && typeof secondaryPhone == 'string';
 
 
