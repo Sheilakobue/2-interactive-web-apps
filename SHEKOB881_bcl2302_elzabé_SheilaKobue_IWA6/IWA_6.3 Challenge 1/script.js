5551cdf6baa1b38340382fa18f3d010f0c7dc9c6
Sheila Kobue <maserole.mojela@gmail.com>
2023-04-04 13:27:54 +0200

    added comments

---
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA6/IWA_6.3 Challenge 1/script.js
@@ -0,0 +1,15 @@
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+//i've noticed that valuables are not numbers but string
+//i have stated by using !NaN to state that the variables and not a number
+// added && and string to state that primaryPhone is a string.
+
+const primaryValid = !isNaN(primaryPhone)&& typeof primaryPhone== 'string';
+
+const secondaryValid = !isNaN(secondaryPhone) && typeof secondaryPhone == 'string';
+
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid )
