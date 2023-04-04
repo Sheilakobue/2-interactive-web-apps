89cbe9db832ad1628c851a5e5de74a8df4f70fdd
Sheila Kobue <maserole.mojela@gmail.com>
2023-04-04 11:09:28 +0200

    added comments

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA5/IWA_5.2 Challenge 1/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA5/IWA_5.2 Challenge 1/script.js
@@ -1,6 +1,10 @@
+//added constant to create variables and ;(semicolons)
 const FREE_WARNING = "Free shipping only applies to single customer orders";
 const BANNED_WARNING = "Unfortunately we do not ship to your country of residence";
 const NONE_SELECTED = '0';
+
+//added let to create variables
+// changed location,currency,customer by adding 1
 
 let location1 ="RSA";
 let currency1 = "R";
@@ -13,17 +17,25 @@ const shirts = 150 * NONE_SELECTED;
 const batteries = 35 * 2;
 const pens = 5 * NONE_SELECTED;
 
+//put everything in brackets, 
+//added '' (apostrophes) & ==(equal signs)
+//added ()parentheses and combined the the sentence with &&
+// created a console for a free-warning feedback
 
-if (location1 === 'RSA' && shipping === 400 && currency1 === 'R'){
+
+if (location1 === 'RSA'){
+  shipping === 400 && currency1 === 'R'
 
     console.log (FREE_WARNING);
 
 }
 if(location1==='NAM'){
-  shipping =600;}
-  {
+  shipping === 600
+  
   console.log(FREE_WARNING);
 }
+// specified shipping for RSA,NAM,NK in an if statement
+//console the BANNED_WARNING)
 
 if (location1 === 'RSA') {
   shipping = 400;
@@ -34,6 +46,10 @@ if (location1 === 'RSA') {
 } else {
   shipping = 800;
 }
+//created a variable for subtotal
+//and an if statement to state that is the order is greater than a 1000 and is in RSA or NAM then the shipping =0
+// or and it will reflect the warning message if shipping is less
+
 const subtotal = shoes + toys + shirts + batteries + pens;
 if ((subtotal >= 1000) && (location1 === 'RSA' || location1 === 'NAM') && (customers === 1)) {
   shipping = 0;
@@ -42,5 +58,8 @@ if ((subtotal >= 1000) && (location1 === 'RSA' || location1 === 'NAM') && (custo
 } else if ((shipping === 0) && (customers1 !== 1)) {
   console.log(FREE_WARNING);
 }
+
+// calculated the subtotal & the shipping to get the total
+// consoled the price
 const total = subtotal + shipping;
 console.log('Price:', currency1, total);
