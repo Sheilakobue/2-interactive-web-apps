403e319cf48ea9c496985f2049d60972012c2979
Sheila Kobue <maserole.mojela@gmail.com>
2023-04-11 23:22:15 +0200

    corrected the code

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA9/IWA_9.2 Challenge 1/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA9/IWA_9.2 Challenge 1/script.js
@@ -1,15 +1,11 @@
 const salary = 4000;
 const lodging = 'apartment'
 const size = 'large'
-
-
 // Only change the syntax below (not the values or key names)
-
 const expenses = {
     food: 51.7501,
     transport:  10.2,
 }
-  
 const tax = {
     734: '3%',
     234: '20%',
@@ -17,7 +13,6 @@ const tax = {
     415: '38%',
     502: '42%',
 }
-
 const rent = {
     none: 0,
     'small-room': 200,
@@ -27,11 +22,9 @@ const rent = {
     'small-house': 1200,
     'large-house': 2400,
 }
-
 // You can change below however you want
-
-const taxAsDecimal = `tax ${913} / 100`
-const startingAfterTax = salary * 1 - taxAsDecimal
-const type = lodging + size
-const balance = expenses(transport) - expenses(food) - expenses(rent.type) 
-console.log(balance)
+const taxAsDecimal = parseInt(tax[913]) / 100
+const startingAfterTax = salary - (salary * taxAsDecimal)
+const type = parseInt(rent['large-apartment'])
+const balance = startingAfterTax - (expenses.transport + expenses.food + type)
+console.log(balance.toFixed(2))
