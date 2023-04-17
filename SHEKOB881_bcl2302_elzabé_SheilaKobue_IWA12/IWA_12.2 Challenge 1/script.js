a9b737f0d1a09715fb78636c1b021fbbb0a23c10
Sheila Kobue <maserole.mojela@gmail.com>
2023-04-17 08:37:52 +0200

    corrected code

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA12/IWA_12.2 Challenge 1/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA12/IWA_12.2 Challenge 1/script.js
@@ -1,62 +1,48 @@
 const STATUS_MAP = {
     shelf: {
-        color: 'green',
-        canReserve: true,
-        canCheckout: true,
-        canCheckIn: false,
+      color: 'green',
+      canReserve: true,
+      canCheckout: true,
+      canCheckIn: false,
     },
     reserved: {
-        color: 'blue',
-        canReserve: false,
-        canCheckout: true,
-        canCheckIn: false,
+      color: 'blue',
+      canReserve: false,
+      canCheckout: true,
+      canCheckIn: false,
     },
     overdue: {
-        color: 'red',
-        canReserve: false,
-        canCheckout: false,
-        canCheckIn: true,
+      color: 'red',
+      canReserve: false,
+      canCheckout: false,
+      canCheckIn: true,
     },
     checkedOut: {
-        color: 'orange',
-        canReserve: false,
-        canCheckout: false,
-        canCheckIn: true,
+      color: 'orange',
+      canReserve: false,
+      canCheckout: false,
+      canCheckIn: true,
     }
-}
-// Edit below line
-const book1 = {
-status: document.querySelector('#book1 .status'),
-reserve: document.querySelector('#book1 .reserve'),
-checkout: document.querySelector('#book1 .checkout'),
-checkin: document.querySelector('#book1 .checkin')
-}
-const book2 = {
-status: document.querySelector('#book2 .status'),
-reserve: document.querySelector('#book2 .reserve'),
-checkout: document.querySelector('#book2 .checkout'),
-checkin: document.querySelector('#book2 .checkin')
-}
-const book3 = {
-    status: document.querySelector('#book3 .status'),
-    reserve: document.querySelector('#book3 .reserve'),
-    checkout: document.querySelector('#book3 .checkout'),
-    checkin: document.querySelector('#book3 .checkin')
-    }
- function updateBookStatus(book) {
-    const status = book.status.textContent.trim()
-    const statusInfo = STATUS_MAP[status]
-    book.status.style.color = statusInfo.color
-    book.reserve.disabled = !statusInfo.canReserve
-    book.checkout.disabled = !statusInfo.canCheckout
-    book.checkin.disabled = !statusInfo.canCheckIn
-}
-updateBookStatus(book1)
-updateBookStatus(book2)
-updateBookStatus(book3)
-const overdue = book1.checkin
-const reserveCheckout = overdue.style.color = ''
-const reserved = book2.checkin
-const reserveCheckin = overdue.style.color = ''
-const shelf = book3.checkin
-const checkIn = shelf.style.color = ''
+  };
+  // Get all the book div elements
+  const bookDivs = document.querySelectorAll('div[id^="book"]');
+  // Loop over each book div element
+  bookDivs.forEach((bookDiv) => {
+    // Get the status span and buttons
+    const statusSpan = bookDiv.querySelector('.status');
+    const reserveBtn = bookDiv.querySelector('.reserve');
+    const checkoutBtn = bookDiv.querySelector('.checkout');
+    const checkinBtn = bookDiv.querySelector('.checkin');
+    // Get the status value and corresponding properties from STATUS_MAP
+    const statusVal = statusSpan.textContent.trim().toLowerCase();
+    const statusProps = STATUS_MAP[statusVal];
+    // Update the color and disabled state of the buttons based on the status properties
+    statusSpan.style.color = statusProps.color;
+    reserveBtn.disabled = !statusProps.canReserve;
+    checkoutBtn.disabled = !statusProps.canCheckout;
+    checkinBtn.disabled = !statusProps.canCheckIn;
+    // Set the button colors to black and grayscale
+    reserveBtn.style.color = reserveBtn.disabled ? 'gray' : 'black';
+    checkoutBtn.style.color = checkoutBtn.disabled ? 'gray' : 'black';
+    checkinBtn.style.color = checkinBtn.disabled ? 'gray' : 'black';
+  });
