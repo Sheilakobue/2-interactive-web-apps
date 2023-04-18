70ff5958131a18156386ff9ddbfbf8f1b7764faa
Sheila Kobue <maserole.mojela@gmail.com>
2023-04-18 19:36:24 +0200

    corrected the code

---
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA14/IWA_14.2 Challenge 1/script.js
@@ -0,0 +1,11 @@
+const firstName = 'John';
+const age = 35;
+const hobbyLes = 'Coding';
+const logTwice = (parameter) => {
+  console.log(parameter);
+  console.log(parameter);
+}
+function hobby () {
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobbyLes}!`)
+};
+hobby(); (edited) 
