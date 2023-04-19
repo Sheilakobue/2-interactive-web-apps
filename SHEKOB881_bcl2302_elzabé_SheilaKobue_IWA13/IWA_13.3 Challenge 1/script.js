38d35e7a7f718bbb487ce365f69aab8e6f549c9a
Sheila Kobue <maserole.mojela@gmail.com>
2023-04-19 09:23:24 +0200

    added comments

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA13/IWA_13.3 Challenge 1/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA13/IWA_13.3 Challenge 1/script.js
@@ -2,17 +2,24 @@ let state = 'idle'
 let user = null
 let calculated = '1'
 // Only allowed to change below
+//added the = to assign a variable
+//used the typeof to check the variable type
+// I used the parseInt to change the calculated variable to a number 
 const logCalc = () => {
     const isString = typeof calculated === 'string';
     const calculatedAsNumber = isString ? parseInt(calculated) : calculated;
     calculated = calculatedAsNumber + 1;
 }
+
+//I've added () parentheses and semicolons;
+
 const calcUser = () => {
   logCalc ();
   if (calculated > 2) user = 'John';
   if (calculated > 2) state = 'requesting';
   if (calculated > 3) state = 'idle';
 }
+// it does not need editing.
 const checkUser = () => {
     if (user && state === 'requesting') {
         console.log(`User: ${user} (${calculated})`);
