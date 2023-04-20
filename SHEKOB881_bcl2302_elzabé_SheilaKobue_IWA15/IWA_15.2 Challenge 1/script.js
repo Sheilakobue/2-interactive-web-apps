ef8a02832724d6c6b7110217cff1e91a9a6fe2e8
Sheila Kobue <maserole.mojela@gmail.com>
2023-04-20 16:18:01 +0200

    corrected code

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA15/IWA_15.2 Challenge 1/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA15/IWA_15.2 Challenge 1/script.js
@@ -1,51 +1,38 @@
-// scripts.js
-
 const data = {
-	lists: [
-		['first', [15, 11, 13, 7, 5]],
-		['second', [2, 6, 8, 4, 14, 12, 10]],
-		['third', [9, 3, 1]],
-	]
+    lists: [
+        ['first', [15, 11, 13, 7, 5]],
+        ['second', [2, 6, 8, 4, 14, 12, 10]],
+        ['third', [9, 3, 1]],
+    ]
 }
-
 // Only edit below
-//you will need to extract the numbers from all three arrays in lists inside the data object and combine all values into a new single array called result
-const { first = 1 } = data.first || {}
-const { second = 1 } = data.second || {}
-const { third = 1 } = data.third || {}
-
+const  first =  data.lists[0][1]
+const  second =  data.lists[1][1]
+const  third = data.lists[2][1]
 const result = []
-
-const extractBiggest = (lists) => {
-	if (first[-1] > second[-1]) {
-		return first
-	}
-
-	if (third[-1] < 1) {
-		return second
-	}
-	
-	return third
+const extractBiggest = () => {
+    if ( first[first.length - 1] >= second[second.length - 1] && first[first.length - 1] >= third[third.length - 1]) {
+        return first.pop()
+    }
+    if
+    (second[second.length - 1] >= third[third.length - 1]) {
+        return second.pop()
+    }
+    return third.pop()
 }
-
 // Only edit above
-
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
-
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
-
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
-result.push(extractBiggest())
-
 console.log(result)
