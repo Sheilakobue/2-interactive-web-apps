60c4863845b13d89fe19e129ad35bd85cfca9a4f
Sheila Kobue <maserole.mojela@gmail.com>
2023-04-20 14:15:22 +0200

    added comments

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA10/IWA_10.2 Challenge 1/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA10/IWA_10.2 Challenge 1/script.js
@@ -49,15 +49,22 @@ const holidays = {
 const christmas = 6
 const futureId = 9
 // Do not change code above this comment
+//check whether an item has been assigned to the 9 key, if so, it should log the name of the holiday. If nothing has been assigned yet, then the following should be logged: ID 9 not created yet
+
+// stored the variable in an array and used the $ to call out the variable.
 console.log(holidays[futureId]|| `ID ${futureId} not created yet`)
+
 // Create a copy of the Christmas object and update the name and date
 const christmasCopy = { ...holidays[6] }
+// updated the christmasCopy variable 
 christmasCopy.name = 'X-mas Day'
 christmasCopy.date.setHours(0)
 christmasCopy.date.setMinutes(0)
+
 // Check if the new date is earlier than the current date and log the result
 const isEarlier = christmasCopy.date.getTime() < holidays[6].date.getTime()
 console.log('New date is earlier:', isEarlier)
+
 // Log the changes made to the copied object
 console.log(`ID change: false`)
 console.log(`Name change: ${christmasCopy.name}`)
---
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA15/IWA_15.2 Challenge 1/script.js
@@ -0,0 +1,51 @@
+// scripts.js
+
+const data = {
+	lists: [
+		['first', [15, 11, 13, 7, 5]],
+		['second', [2, 6, 8, 4, 14, 12, 10]],
+		['third', [9, 3, 1]],
+	]
+}
+
+// Only edit below
+//you will need to extract the numbers from all three arrays in lists inside the data object and combine all values into a new single array called result
+const { first = 1 } = data.first || {}
+const { second = 1 } = data.second || {}
+const { third = 1 } = data.third || {}
+
+const result = []
+
+const extractBiggest = (lists) => {
+	if (first[-1] > second[-1]) {
+		return first
+	}
+
+	if (third[-1] < 1) {
+		return second
+	}
+	
+	return third
+}
+
+// Only edit above
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+console.log(result)
