ed2b876a793c62b6e05b00c87da30d33a312a65c
Sheila Kobue <maserole.mojela@gmail.com>
2023-04-21 11:47:22 +0200

    added comments

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA15/IWA_15.2 Challenge 1/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA15/IWA_15.2 Challenge 1/script.js
@@ -1,13 +1,17 @@
 const data = {
-    lists: [
-        ['first', [15, 11, 13, 7, 5]],
-        ['second', [2, 6, 8, 4, 14, 12, 10]],
-        ['third', [9, 3, 1]],
-    ]
+	lists: [
+		['first', [15, 11, 13, 7, 5]],
+		['second', [2, 6, 8, 4, 14, 12, 10]],
+		['third', [9, 3, 1]],
+	]
 }
+
 // Only edit below
+
+// used length-1 to get the last item
+// pop()remove the last item of the first variable
 const  first =  data.lists[0][1]
-const  second =  data.lists[1][1]
+const  second =  data.lists[1 ][1]
 const  third = data.lists[2][1]
 const result = []
 const extractBiggest = () => {
@@ -20,19 +24,26 @@ const extractBiggest = () => {
     }
     return third.pop()
 }
+
+
 // Only edit above
+
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
+
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
+
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
+result.push(extractBiggest())
+
 console.log(result)
