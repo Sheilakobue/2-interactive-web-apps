d8d08cf7724cce8e1530c434ef90180471b12329
Sheila Kobue <maserole.mojela@gmail.com>
2023-04-24 11:59:51 +0200

    created file

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA15/IWA_15.2 Challenge 1/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA15/IWA_15.2 Challenge 1/script.js
@@ -7,14 +7,17 @@ const data = {
 }
 
 // Only edit below
-
-// used length-1 to get the last item
-// pop()remove the last item of the first variable
+//i define three variables that extract the second element from the first, second, third arrays within the data.list
 // i have also used an arrow function
 const  first =  data.lists[0][1]
 const  second =  data.lists[1 ][1]
 const  third = data.lists[2][1]
 const result = []
+
+//We will need to ensure that the extractBiggest function looks at the last item in each of the three lists and selects the biggest one.
+
+// pop()remove the last item of the list and return it when its called out
+// used length-1 to get the last item
 const extractBiggest = () => {
     if ( first[first.length - 1] >= second[second.length - 1] && first[first.length - 1] >= third[third.length - 1]) {
         return first.pop()
