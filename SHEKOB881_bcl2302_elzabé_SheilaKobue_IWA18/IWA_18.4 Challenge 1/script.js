53e5bdf6876db6115ae75d0f301d23243af3e8cd
Sheila Kobue <maserole.mojela@gmail.com>
2023-04-28 15:41:47 +0200

    moved index file

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA18/IWA_18.4 Challenge 1/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_IWA18/IWA_18.4 Challenge 1/script.js
@@ -1,27 +1,12 @@
 // scripts.js
 
+/* Gets and formats the current hour and minute values */
+
+const hour = new Date().getHours().toString().padStart(2, '0')
+const minutes = new Date().getMinutes().toString().padStart(2, '0')
+const seconds = new Date().getSeconds().toString().padStart(2, '0')
+
+/* Gets the paragraph HTML node and changes the text inside to show the above values */
+
 const node = document.querySelector('p')
-const button = document.querySelector('button')
-
-/** A function takes an action name and returns a string with the action and time */
-const generateMessage = (action) => {
-    const hour = new Date().getHours().toString().padStart(2, '0')
-    const minutes = new Date().getMinutes().toString().padStart(2, '0')
-    const seconds = new Date().getSeconds().toString().padStart(2, '0')
-    return `JavaScript ${action} at ${hour}:${minutes}:${seconds}`
-}
-
-/** A function that generates an update message and adds it to the paragraph */
-const updateText = () => {
-    const updateMessage = generateMessage('updated')
-    node.innerText = updateMessage
-}
-
-/* Creates initial message automatically without waiting for an event */
-
-const initialMessage = generateMessage('ran')
-node.innerText = initialMessage
-
-/* Creates event listener that fires `updateText` (handler) when button is clicked */
-
-button.addEventListener('click', updateText)
+node.innerText = `JavaScript ${action} at ${hour}:${minutes}:${seconds}`
