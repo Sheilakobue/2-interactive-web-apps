7f6e10ca969edf39230674a6030e8b02111ed851
Sheila Kobue <maserole.mojela@gmail.com>
2023-05-02 16:30:03 +0200

    added paragraph tag

--- My projects/script2.js
+++ My projects/script2.js
@@ -2,16 +2,15 @@
 //Listen for clicks on the increment button
 //increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
-
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let count = 0
 
 console.log(countEl)
 
-let count = 0
 
 function increment() {
-
-    count = count + 1
+    count += 1
     countEl.innerText = count  
     
 }
