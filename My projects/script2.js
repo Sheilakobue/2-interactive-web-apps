c1378b0776ecc77fc7a5049b3d3488c88356fe02
Sheila Kobue <maserole.mojela@gmail.com>
2023-05-02 16:13:32 +0200

    added save button

--- My projects/script2.js
+++ My projects/script2.js
@@ -3,7 +3,7 @@
 //increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 
-let countEl = document.getElementById("counter-el")
+let countEl = document.getElementById("count-el")
 
 console.log(countEl)
 
